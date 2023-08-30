aed91448b2e8e720990b27ce8d8ec0f7f664fe67
Peacebern <peace8282bern@gmail.com>
2023-08-30 12:30:27 +0100

    send message functionality not working previously as variables were outside the event listener function

--- script/index.js
+++ script/index.js
@@ -1,3 +1,4 @@
+document.getElementById('submit').addEventListener('click', function () {
     const name = document.getElementById('name');
     const email = document.getElementById('email');
     const message = document.getElementById('message');
@@ -5,75 +6,71 @@
     const mailError = document.getElementById('mailError');
     const messageError = document.getElementById('messageError');
 
-    const senderName = name.value
-    const senderEmail = email.value
-    const msgText = message.value
+    nameError.textContent = "";
+    mailError.textContent = "";
+    messageError.textContent = "";
+
+    let hasError = false;
+
+    const senderName = name.value;
+    const senderEmail = email.value;
+    const msgText = message.value;
 
     const serviceID = "service_av6iq8l";
     const templateID = "template_tgfkcge";
 
     const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
 
-document.getElementById('submit').addEventListener('click', function () {
-
-;
-
-    nameError.textContent = ""
-    mailError.textContent = ""
-    messageError.textContent = ""
-
-    let hasError = false
-
     if (senderName.trim() === "") {
-        nameError.textContent = "Name is required"
-        hasError = true
+        nameError.textContent = "Name is required";
+        hasError = true;
     }
 
     if (senderEmail.trim() === "") {
-        mailError.textContent = "Email is required"
-        hasError = true
+        mailError.textContent = "Email is required";
+        hasError = true;
     } 
 
     if (!emailRegex.test(senderEmail)) {
-        mailError.textContent = "Input a valid email address"
-        hasError = true
+        mailError.textContent = "Input a valid email address";
+        hasError = true;
     }
 
     if (msgText.trim() === "") {
-        messageError.textContent = "Type in your message"
-        hasError = true
+        messageError.textContent = "Type in your message";
+        hasError = true;
     }
 
     if (hasError) {
-        return
+        return;
     }
 
     const emailJSVariable = {
         name: senderName,
         email: senderEmail,
         message: msgText
-    }
+    };
 
-emailjs.send("service_av6iq8l", "template_tgfkcge", emailJSVariable)
-    .then( res => {
-        alert("Thank you,! Your message has been received");
+    emailjs.send("service_av6iq8l", "template_tgfkcge", emailJSVariable)
+        .then(res => {
+            alert("Thank you! Your message has been received");
+            name.value = "";
+            email.value = "";
+            message.value = "";
+        })
+        .catch(error => {
+            console.error("An error occurred:", error);
+        });
+});
 
-         name.value = ""
-    const senderEmail = email.value
-    const msgText = message.value
-    })
-    .catch(error);
+document.getElementById('name').addEventListener('input', function () {
+    document.getElementById('nameError').textContent = "";
+});
 
-})
+document.getElementById('email').addEventListener('input', function () {
+    document.getElementById('mailError').textContent = "";
+});
 
-name.addEventListener('input', function () {
-    nameError.textContent = ""
-})
-
-email.addEventListener('input', function () {
-    mailError.textContent = ""
-})
-
-message.addEventListener('input', function () {
-    messageError.textContent = ""
-})
+document.getElementById('message').addEventListener('input', function () {
+    document.getElementById('messageError').textContent = "";
+});
